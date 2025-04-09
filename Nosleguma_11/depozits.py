fe86906f7c61c68f9700dd30e625aa7a2b4b075a
Trip1ns <renars.veidemanis@r6vsk.lv>
2025-04-09 15:39:06 +0300

    STRADA

--- Nosleguma_11/depozits.py
+++ Nosleguma_11/depozits.py
@@ -11,25 +11,41 @@ try:
 except:
     pass  
 
-materials = input("Ievadiet materiālu (Plastmasa, Skardene, Stikla): ")
-Skaits = int(input('Cik pudeles nodosiet? '))
+def aprekinasana():
+    kopsumma = 0
+    while True:
+        materials = input("Ievadiet materiālu (Plastmasa, Skardene, Stikla): ")
+        if materials not in materialuVeidi:
+            input("Ievadiet pareizu pudeļu materiālu:")
+        Skaits = int(input('Cik pudeles nodosiet? '))
+        if Skaits < 0:
+            int(input("Ievadiet pozitīvu skaitu:"))
+            
+        if materials in materialuVeidi:
+            depozits = {}
+            numurs = str(uuid.uuid4())
+            cena = materialuVeidi[materials]
+            summa = cena * Skaits
+            kopsumma += summa
+            print(kopsumma)
+            depozits["Numurs"] = numurs
+            depozits["Materials"] = materials
+            depozits["Skaits"] = Skaits
+            depozits["Summa"] = summa
 
-if materials in materialuVeidi:
-    depozits = {}
-    numurs = str(uuid.uuid4())
-    cena = materialuVeidi[materials]
-    summa = cena * Skaits
-    print(f"Kopējā summa: {summa} EUR")
-    depozits["Numurs"] = numurs
-    depozits["Materials"] = materials
-    depozits["Skaits"] = Skaits
-    depozits["Summa"] = summa
+            visiDati.append(depozits)
 
-    visiDati.append(depozits)
+        velreiz = input("Vai tev vēl ir depozīts?(J/N)").capitalize()   
+        if velreiz == "J":
+            pass
+        elif velreiz =="N":
+            print(f"Depozīta atmaksa:{kopsumma}")
+            break
+        else:
+            input("Vai tev vēl ir depozīts?(J/N)")
 
-    dati = json.dumps(visiDati, ensure_ascii=False)
-    a = f"Nosleguma_11/depozita_dati.json"
-    with open(a, "w", encoding="utf-8") as datne:
-        datne.write(dati)
-else:
-    print("Nederīgs materiāla veids! Lūdzu, izvēlieties no: Plastmasa, Skardene, Stikla.")
+        dati = json.dumps(visiDati, ensure_ascii=False)
+        a = f"Nosleguma_11/depozita_dati.json"
+        with open(a, "w", encoding="utf-8") as datne:
+            datne.write(dati)
+aprekinasana()
