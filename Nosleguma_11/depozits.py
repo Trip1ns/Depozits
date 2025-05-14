9444a732a20f2afc58fdc88b1fb4ca5266116b66
Ernests <ernests.simcuks@r6vsk.lv>
2025-05-14 14:46:43 +0300

    Revert "Merge branch 'main' of https://github.com/Trip1ns/Depozits"
    This reverts commit 6ddeb5aff3cad0eb12ff558d100aca04cb955f7f, reversing
    changes made to bbdceaf423b0284aafc8d1143bf034ef4de9ec68.

--- Nosleguma_11/depozits.py
+++ Nosleguma_11/depozits.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import tkinter
 
 
-visiDati = []
-materialuVeidi = {'Plastmasa': 0.1, 'Skardene': 0.2, 'Stikla': 0.5}
+tagad = datetime.now() 
+apalots = tagad.replace(second=2, microsecond=0)
 
 visiDati = {"saraksts": [], "kopsumma": 0}
 materialuVeidi = {1: 0.1, 2: 0.2, 3: 0.5} # | 1 - Plastmasa | 2 - Skārdene | 3 - Stikla |
@@ -20,30 +20,43 @@ def aprekinasana(): #izveidot atiecīgo materiālu ciparu notiek summas un kopsu
     kopsumma = 0
 
     while True:
-        materials = input("Ievadiet materiālu (Plastmasa, Skardene, Stikla): ")
-        if materials not in materialuVeidi:
-            input("Ievadiet pareizu pudeļu materiālu:")
-        Skaits = int(input('Cik pudeles nodosiet? '))
-        if Skaits < 0:
-            int(input("Ievadiet pozitīvu skaitu:"))
-            
-        if materials in materialuVeidi:
-            depozits = {}
-            numurs = str(uuid.uuid4())
-            cena = materialuVeidi[materials]
-            summa = cena * Skaits
-            kopsumma += summa
-            print(kopsumma)
-            depozits["Numurs"] = numurs
-            depozits["Materials"] = materials
-            depozits["Skaits"] = Skaits
-            depozits["Summa"] = summa
+        try:
+            materials = int(input("Ievadiet materiālu (1 - Plastmasa, 2 - Skārdene, 3 - Stikla): ")) # Tiek pieprasīts ievadīt materiāla tipu, ja tiek ievadits materiala cipariņš kas netiek piedāvāts, kommanda pieprasa ievadīt pareizu materiāla veidu.
+            if materials not in materialuVeidi:
+                print("Ievadiet pareizu pudeļu materiālu!")
+                continue
+        except ValueError:
+            print("Ievadi skaitli 1, 2 vai 3")
+            continue    
+
+        try:
+            Skaits = int(input("Cik pudeles nodosiet?")) # Depozīta nododot jāievada pudeļu skaits cik tiek nodots, ja skaits ir negatīvs progrāma liek skaitli ievadīt atkārtoti.
+            if Skaits <= 0:
+                print("Ievadiet pozitīvu skaitu!")
+                continue
+        except ValueError:
+            print("Lūdzu ievadiet skaitli!")
+            continue
+
+        depozits = {}
+        nodosanas_laiks = str(datetime.now().replace(second=2, microsecond=0))
+        cena = materialuVeidi[materials]
+        summa = cena * Skaits
+        kopsumma += summa
+
+        depozits["Nodošanas laiks"] = nodosanas_laiks
+        depozits["Materials"] = materials
+        depozits["Skaits"] = Skaits
+        depozits["Summa"] = summa
 
         visiDati["saraksts"].append(depozits)   
         visiDati["kopsumma"] += summa
         materialusumma[materials] = materialusumma.get(materials, 0) + summa
 
-        velreiz = input("Vai tev vēl ir depozīts?(J/N)").capitalize()   
+        velreiz = input("Vai tev vēl ir depozīts?(J/N): ").capitalize() # Komanda piedāvā nodot vēl depozītu neapturot iepriekšējo nodoto materiālu summu ja izvelies J, proggrama pieprasa ievadīt materiālu un skaitu ja uzspied N, kommanda parāda cik depozīta atmaksa tev pienākās.
+        if velreiz == "N":
+            print(f"Depozīta atmaksa: {round(kopsumma, 2)} EUR")
+            break
         if velreiz == "J":
             continue
         else:
